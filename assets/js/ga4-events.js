a718f0786f0a6a03970fdb54da36bcb838a42ebf
h1994st <h1994st@gmail.com>
2022-06-02 16:39:16 -0400

    Update

---
+++ assets/js/ga4-events.js
@@ -0,0 +1,27 @@
+/**
+ *
+ * @authors Shengtuo Hu (h1994st@gmail.com)
+ * @date    2022-06-02 16:29:04
+ * @version 1.0
+ */
+
+ (function(window, document) {
+    function handleOutboundLinkClicks(event) {
+      gtag('event', 'click', {
+        event_category: 'Custom',
+        event_label: event.target.dataset.label,
+        value: navigator.userAgent
+      });
+    }
+
+    document.querySelectorAll('a[data-label]').forEach(function (element) {
+      element.onclick = handleOutboundLinkClicks;
+    });
+
+    gtag('event', 'page_view', {
+      event_category: 'Custom',
+      event_label: 'Loaded',
+      user_agent: navigator.userAgent,
+      value: 'Datetime: ' + (new Date()).toString()
+    });
+  })(window, document);
