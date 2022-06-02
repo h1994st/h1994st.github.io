e13c7a8a4790c37e9222bd98b83cea5f9df41563
h1994st <h1994st@gmail.com>
2022-06-02 16:52:38 -0400

    Update

--- assets/js/ga4-events.js
+++ assets/js/ga4-events.js
@@ -5,23 +5,30 @@
  * @version 1.0
  */
 
- (function(window, document) {
-    function handleOutboundLinkClicks(event) {
-      gtag('event', 'click', {
-        event_category: 'Custom',
-        event_label: event.target.dataset.label,
-        value: navigator.userAgent
-      });
-    }
+(function(window, document) {
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  gtag('js', new Date());
+  gtag('config', 'G-ZMH6NLL7C7');
 
-    document.querySelectorAll('a[data-label]').forEach(function (element) {
-      element.onclick = handleOutboundLinkClicks;
-    });
-
-    gtag('event', 'page_view', {
+  function handleOutboundLinkClicks(event) {
+    gtag('event', 'click', {
       event_category: 'Custom',
-      event_label: 'Loaded',
-      user_agent: navigator.userAgent,
-      value: 'Datetime: ' + (new Date()).toString()
+      event_label: event.target.dataset.label,
+      value: navigator.userAgent
     });
-  })(window, document);
+  }
+
+  document.querySelectorAll('a[data-label]').forEach(function (element) {
+    element.onclick = handleOutboundLinkClicks;
+  });
+
+  gtag('event', 'page_view', {
+    event_category: 'Custom',
+    event_label: 'Loaded',
+    user_agent: navigator.userAgent,
+    value: 'Datetime: ' + (new Date()).toString()
+  });
+})(window, document);
